23eef055d6c4f37fb3933f4ae9598c85dc99cfa0
sw <sw@sws-MacBook-Pro.local>
2020-03-08 11:40:22 -0600

    Added ui and basic connection to the projects, projects-list and project-details components

--- apps/sw14/src/app/projects/projects.component.ts
+++ apps/sw14/src/app/projects/projects.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project, ProjectsService } from '@bb/core-data';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bb-projects',
@@ -6,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  projects$: Project[] = [
+    {id: '1', title: 'Proj One', details: 'Some details', importanceLevel: 5}
+  ];
+  project: Project;
+  selectedProject: Project;
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    private projectsService: ProjectsService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  select(project: Project) {
+    this.selectedProject = project;
+    this.formGroup.patchValue(project);
+  }
+
+  delete(project: Project) {
+console.log('Deleting ...')
+  }
+
+  save(project: Project) {
+    console.log('Saving ...')
+
+  }
+
+  reset() {
+    console.log('Resetting ...')
+
+  }
+
+
+
+
+  initForm() {
+    this.formGroup = this.formBuilder.group({
+      id: [null],
+      title: ['', Validators.compose([
+        Validators.required
+      ])],
+      details: ['', Validators.compose([
+        Validators.required
+      ])],
+      importanceLevel: [0]
+    })
+  }
 }
